1a28aef0a5facb008fb550035564bc767264b816
Fabio Zunta <nonameeesg@gmail.com>
2022-11-17 01:21:34 -0300

    having a hard time, trying to make the buttons store the values in the textarea

--- calculator.js
+++ calculator.js
@@ -1,7 +1,9 @@
 const display = document.body.querySelector(".display");
 const clr = document.body.querySelector("#clr");
 
-clr.addEventListener("click", function(){display.textContent = "CLEAR"});
+clr.addEventListener("click", function(){
+    display.textContent = "CLEAR"
+});
 
 const zero = document.body.querySelector("#zero");
 const one = document.body.querySelector("#one");
@@ -20,4 +22,28 @@ const subtract = document.body.querySelector("#subtract");
 const divide = document.body.querySelector("#divide");
 const multiply = document.body.querySelector("#multiply");
 
-// todo: maybe I can creat a loop instead of declaring 15 constants...
+var inputTextArea = []; // using an array to store input
+
+zero.addEventListener("click", function(){
+    inputTextArea.push(0);
+    display.textContent = inputTextArea; // problem: it pushes to the array instantly. How can I push two digits or more into the array?
+});
+
+add.addEventListener("click", function(){
+    display.textContent = "+";
+});
+
+// todo: maybe I can creat a loop instead of declaring 17 constants...
+
+var buttonsLength = document.body.querySelectorAll("input").length;
+var buttonSelector = document.body.querySelectorAll("input");
+
+/*
+
+for(i = 0; i <= buttonsLength; i++){
+    buttonSelector[i].
+}
+
+maybe... I need a while loop there.
+
+*/
