c3cb230ec5c6a254ce45a296d276bfaf3b7ffc0d
Fabio Zunta <nonameeesg@gmail.com>
2022-11-23 21:36:18 -0300

    finally understood the calculator logic

--- calculator.js
+++ calculator.js
@@ -1,10 +1,59 @@
-var resultNum, theNum = "";
+var oldNum, resultNum, theNum = "";
+
+var opsLength = document.querySelectorAll(".ops").length;
 var numLength = document.querySelectorAll(".num").length;
 
-// Add click event to numbers
-for (var i = 0; i <= numLength; i++) {
-    document.querySelectorAll(".num")[i].addEventListener("click", function(){
-        theNum = theNum + this.getAttribute("data-num");
+// click event on numbers
+for (var i = 0; i < numLength; i++) {
+    document.querySelectorAll(".num")[i].onclick = function() {
+        theNum += this.getAttribute("data-num");
         viewer.innerHTML = theNum;
-    });
-};
+    };
+}
+
+var clear = function(){
+    theNum = ""
+    viewer.innerHTML = theNum + "<br>"
+}
+// click event on clear button
+document.querySelector("#clear").onclick = clear;
+
+// click event on operators buttons
+for (var i = 0, l = opsLength; i < l; i++) {
+    document.querySelectorAll(".ops")[i].onclick = function() {
+      oldNum = theNum;
+      theNum = "";
+      operator = this.getAttribute("data-ops");
+      equals.setAttribute("data-result", "");
+    };
+}
+
+// doing the operations
+var showResult = function(){
+    oldNum = parseFloat(oldNum);
+    theNum = parseFloat(theNum);
+    switch (operator) {
+        case "add":
+          resultNum = oldNum + theNum;
+          break;
+        case "minus":
+          resultNum = oldNum - theNum;
+          break;
+        case "multiply":
+          resultNum = oldNum * theNum;
+          break;
+        case "divide":
+          resultNum = oldNum / theNum;
+          break;
+        default:
+          resultNum = theNum;
+    }
+    
+    viewer.innerHTML = resultNum;
+    equals.setAttribute("data-result", resultNum);
+
+    oldNum = 0;
+    theNum = resultNum;
+}
+// click event on equals button
+document.querySelector("#equals").onclick = showResult;
